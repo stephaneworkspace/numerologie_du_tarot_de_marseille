39b690d098133da8cd31e0a33dd1c88527729a60
Stéphane Bressani <stephane@bressani.dev>
2025-06-01 16:22:11 +0200

    ok

--- Sources/Exec/main.swift
+++ Sources/Exec/main.swift
@@ -7,4 +7,37 @@ import SwiftUI
 import Numerologie_Du_Tarot_De_Marseille_Bressani_Dev
 
 print(Message.hello())
-print("h")
+
+//
+
+let controller = LameMajeuresController()
+
+let semaphore = DispatchSemaphore(value: 0)
+
+controller.getAllLamesMajeures { result in
+    switch result {
+    case .success(let lames):
+        for lame in lames {
+            print("\(lame.id): \(lame.cartouche)")
+        }
+    case .failure(let error):
+        print("Erreur: \(error)")
+    }
+    semaphore.signal()
+}
+
+semaphore.wait()
+
+//
+
+controller.getLameMajeure(id: 1) { result in
+    switch result {
+    case .success(let lame):
+        print("Lame 1 → \(lame.nombre_romain): \(lame.cartouche)")
+    case .failure(let error):
+        print("Erreur (lame id 1): \(error)")
+    }
+    semaphore.signal()
+}
+
+semaphore.wait()
